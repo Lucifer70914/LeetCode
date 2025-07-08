540197a231b5c65a03e0cefd88fed099bf77230a
Lucifer70914 <juug22btech53592@gmail.com>
2025-07-08 17:00:29 +0530

    Time: 48 ms (82.69%), Space: 120.8 MB (97.6%) - LeetHub

--- 1851-maximum-number-of-events-that-can-be-attended-ii/1851-maximum-number-of-events-that-can-be-attended-ii.java
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/1851-maximum-number-of-events-that-can-be-attended-ii.java
@@ -2,26 +2,28 @@ import java.util.*;
 
 class Solution {
     public int maxValue(int[][] events, int k) {
-        Arrays.sort(events, Comparator.comparingInt(a -> a[1]));
+        Arrays.sort(events, (a, b) -> Integer.compare(a[1], b[1]));
         int n = events.length;
+        int[] ends = new int[n];
+        for (int i = 0; i < n; i++) ends[i] = events[i][1];
         int[][] dp = new int[n + 1][k + 1];
 
         for (int i = 1; i <= n; i++) {
-            int[] curr = events[i - 1];
-            int prev = binarySearch(events, curr[0], i - 1);
+            int start = events[i - 1][0], value = events[i - 1][2];
+            int prev = upperBound(ends, start - 1);
             for (int j = 1; j <= k; j++) {
-                dp[i][j] = Math.max(dp[i - 1][j], dp[prev + 1][j - 1] + curr[2]);
+                dp[i][j] = Math.max(dp[i - 1][j], dp[prev + 1][j - 1] + value);
             }
         }
 
         return dp[n][k];
     }
 
-    private int binarySearch(int[][] events, int start, int hi) {
-        int lo = 0, res = -1;
+    private int upperBound(int[] ends, int target) {
+        int lo = 0, hi = ends.length - 1, res = -1;
         while (lo <= hi) {
-            int mid = (lo + hi) / 2;
-            if (events[mid][1] < start) {
+            int mid = (lo + hi) >>> 1;
+            if (ends[mid] < target + 1) {
                 res = mid;
                 lo = mid + 1;
             } else {
