766c5f1472d0162c32520750b2c4f596f153c573
Lucifer70914 <juug22btech53592@gmail.com>
2025-07-08 17:02:12 +0530

    Time: 47 ms (85.58%), Space: 120.1 MB (99.04%) - LeetHub

--- 1851-maximum-number-of-events-that-can-be-attended-ii/1851-maximum-number-of-events-that-can-be-attended-ii.java
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/1851-maximum-number-of-events-that-can-be-attended-ii.java
@@ -2,28 +2,28 @@ import java.util.*;
 
 class Solution {
     public int maxValue(int[][] events, int k) {
+        Arrays.sort(events, (a, b) -> Integer.compare(a[1], b[1]));
         int n = events.length;
-        Arrays.sort(events, (a, b) -> a[1] - b[1]);
         int[] ends = new int[n];
         for (int i = 0; i < n; i++) ends[i] = events[i][1];
+        int[][] dp = new int[n + 1][k + 1];
 
-        int[][] dp = new int[k + 1][n + 1];
-
-        for (int i = 1; i <= k; i++) {
-            for (int j = 1; j <= n; j++) {
-                int idx = upperBound(ends, events[j - 1][0] - 1);
-                dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][idx + 1] + events[j - 1][2]);
+        for (int i = 1; i <= n; i++) {
+            int start = events[i - 1][0], value = events[i - 1][2];
+            int prev = upperBound(ends, start - 1);
+            for (int j = 1; j <= k; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[prev + 1][j - 1] + value);
             }
         }
 
-        return dp[k][n];
+        return dp[n][k];
     }
 
     private int upperBound(int[] ends, int target) {
         int lo = 0, hi = ends.length - 1, res = -1;
         while (lo <= hi) {
             int mid = (lo + hi) >>> 1;
-            if (ends[mid] <= target) {
+            if (ends[mid] < target + 1) {
                 res = mid;
                 lo = mid + 1;
             } else {
