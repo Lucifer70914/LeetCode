3c136cf6e6e1ab9f551cd80f03737dfbdb15806b
Ritesh Kumar Pradhan <juug22btech53592@gmail.com>
2026-01-04 21:02:25 +0530

    Time: 9 ms (97.47%), Space: 46.6 MB (33.08%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.java
+++ 1390-four-divisors/1390-four-divisors.java
@@ -1,35 +1,33 @@
 class Solution {
     public int sumFourDivisors(int[] nums) {
-        int result = 0;
-
-        for (int n : nums) {
-            int sum = 0;
-            int count = 0;
-
-            for (int i = 1; i * i <= n; i++) {
-                if (n % i == 0) {
-                    int d1 = i;
-                    int d2 = n / i;
-
-                    if (d1 == d2) {
-                        count++;
-                        sum += d1;
-                    } else {
-                        count += 2;
-                        sum += d1 + d2;
-                    }
-
-                    if (count > 4) {
-                        break;
-                    }
+        int res=0;
+        for(int n:nums)
+        {
+            int sum=0;
+            int count=0;
+            for(int i=1;i*i<=n;i++)
+            {
+               if(n%i==0)
+               {
+                int d1=i;
+                int d2=n/i;
+                if(d1==d2)
+                {
+                    count+=1;
+                    sum+=d1;
                 }
+                else
+                {
+                    count+=2;
+                    sum+=d1+d2;
+                }
+                if(count>4) break;
+               }
+               
             }
-
-            if (count == 4) {
-                result += sum;
-            }
+            if(count==4) res+=sum;
         }
-
-        return result;
+        return res;
+        
     }
 }
